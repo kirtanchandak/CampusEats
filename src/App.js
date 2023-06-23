fc7affee39693f39f47aaa44ed5ffeef7739c330
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-23 16:45:05 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
