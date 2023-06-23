7eb5aa1b0620b57ddc675c266f1f9bbb60019483
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-23 18:43:09 +0530

    register api endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { userRouter } from "./routes/users.js";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.use("/auth", userRouter);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.listen(5000, () => {
