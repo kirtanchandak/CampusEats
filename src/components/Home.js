46e22903826099254fc52a9f8f36ab2f567e2c87
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-23 15:15:54 +0530

    ui fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
   return (
     <>
       <Layout>
-        <section class="bg-white dark:bg-gray-900">
-          <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-24">
-            <div class="mr-auto place-self-center lg:col-span-7">
+        <section class="">
+          <div class="grid max-w-screen-xl px-4 pt-24 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-24">
+            <div class="mr-auto place-self-center lg:col-span-7 mt-2">
               <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
                 Building digital <br />
                 products & brands.
@@ -18,7 +18,7 @@ function Home() {
                 the utility classes from Tailwind CSS and based on the
                 components from the Flowbite Library Blocks System .
               </p>
-              <div class="sm:flex sm:space-y-0 sm:space-x-4 flex gap-5 pt-3">
+              <div class="sm:flex sm:space-y-0 sm:space-x-4 flex gap-5 pt-7">
                 <button className="font-medium">Login</button>
                 <button className="bg-[#D6FF79] px-2 py-2 rounded-md font-medium">
                   Register as a Business
@@ -26,7 +26,7 @@ function Home() {
               </div>
             </div>
             <div class="lg:mt-0 lg:col-span-5 lg:flex pt-4">
-              <img src={boy} alt="hero image" />
+              <img src={boy} alt="hero-img" />
             </div>
           </div>
         </section>
