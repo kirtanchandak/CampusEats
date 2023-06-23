4dde85f2078893bf9d3447b51a2ae04e20bbbe95
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-23 15:40:23 +0530

    our patners

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Layout from "./Layout";
 import boy from "../assets/Take Away-cuate.png";
+import bits from "../assets/bits.gif";
+import vit from "../assets/vit.png";
+import lmniit from "../assets/lmniit.png";
+import lpu from "../assets/lpu.png";
 
 function Home() {
   return (
@@ -9,7 +13,7 @@ function Home() {
         <section class="">
           <div class="grid max-w-screen-xl px-4 pt-24 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-24">
             <div class="mr-auto place-self-center lg:col-span-7 mt-2">
-              <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+              <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl">
                 Building digital <br />
                 products & brands.
               </h1>
@@ -18,17 +22,38 @@ function Home() {
                 the utility classes from Tailwind CSS and based on the
                 components from the Flowbite Library Blocks System .
               </p>
-              <div class="sm:flex sm:space-y-0 sm:space-x-4 flex gap-5 pt-7">
+              <div class="sm:flex sm:space-y-0 sm:space-x-4 flex gap-5 pt-4">
                 <button className="font-medium">Login</button>
                 <button className="bg-[#D6FF79] px-2 py-2 rounded-md font-medium">
                   Register as a Business
                 </button>
               </div>
             </div>
-            <div class="lg:mt-0 lg:col-span-5 lg:flex pt-4">
+            <div class="lg:mt-0 lg:col-span-5 lg:flex pt-7">
               <img src={boy} alt="hero-img" />
             </div>
           </div>
+          {/* Brands Section */}
+          <section class="flex flex-col justify-center items-center">
+            <h1 className="text-2xl font-semibold">Our Patners:</h1>
+            <div class="max-w-screen-xl px-4 pb-8 mx-auto lg:pb-16 pt-5">
+              <div class="grid grid-cols-2 gap-8  sm:gap-12 sm:grid-cols-3 lg:grid-cols-4">
+                <a href="#" class="flex items-center lg:justify-center">
+                  <img src={bits} alt="" srcset="" className="md:w-48 w-32" />
+                </a>
+                <a href="#" class="flex items-center lg:justify-center">
+                  <img src={vit} alt="" srcset="" className="md:w-48 w-32" />
+                </a>
+                <a href="#" class="flex items-center lg:justify-center">
+                  <img src={lmniit} alt="" srcset="" className="md:w-48 w-32" />
+                </a>
+
+                <a href="#" class="flex items-center lg:justify-center">
+                  <img src={lpu} alt="" srcset="" className="md:w-48 w-32" />
+                </a>
+              </div>
+            </div>
+          </section>
         </section>
       </Layout>
     </>
