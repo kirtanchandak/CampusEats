32e651498dae87df8a49a57426d9e7c638ac89ea
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-25 02:03:36 +0530

    subscription fetched in profile console.log

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Profile() {
   const name = window.localStorage.getItem("name");
+  const userID = window.localStorage.getItem("userID");
   const college = window.localStorage.getItem("college");
   const [cookies, setCookies] = useCookies(["access_token"]);
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    setCookies(["access_token"], "");
+    setCookies("");
     window.localStorage.removeItem("userID");
     navigate("/login");
   };
+
+  useEffect(() => {
+    const fetchSubscriptions = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/saveSubscription/${userID}`
+        );
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchSubscriptions();
+  });
+
   return (
     <>
       <div>
