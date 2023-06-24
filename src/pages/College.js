4ee518a6702e0e1c6b3210fac32960d20047f4f9
Sarvadnya Chavhan <sarvadnyachavhan07@outlook.com>
2023-06-24 05:53:35 +0530

    mapped shops v1

--- src/pages/College.js
+++ src/pages/College.js
@@ -1,15 +1,41 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const College = () => {
+function ShopListBySlug() {
+  const [shops, setShops] = useState([]);
   const { slug } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/colleges/get`);
+        const colleges = response.data;
+        const college = colleges.find((col) => col.slug === slug);
+        if (college) {
+          setShops(college.shops);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [slug]);
+
   return (
     <div>
-      <h1>College</h1>
-      <h1>{slug}</h1>
+      <h2>Shops:</h2>
+      <ul>
+        {shops.map((shop) => (
+          <li key={shop._id.$oid}>
+            <h3>{shop.name}</h3>
+            <p>{shop.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
-export default College;
+export default ShopListBySlug;
