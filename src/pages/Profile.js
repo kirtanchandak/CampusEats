7d8480ce9e990f0babfc0bea01e0e59a7013b3dd
Sarvadnya Chavhan <sarvadnyachavhan07@outlook.com>
2023-06-25 13:18:33 +0530

    added subscription cards to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import Layout from "../components/Layout";
 import axios from "axios";
 
 function Profile() {
   const name = window.localStorage.getItem("name");
   const userID = window.localStorage.getItem("userID");
   const college = window.localStorage.getItem("college");
-  const [cookies, setCookies, removeCookie] = useCookies(["access_token"]);
+  const [, , removeCookie] = useCookies(["access_token"]); // Update this line
   const navigate = useNavigate();
   const [subscriptions, setSubscriptions] = useState([]);
 
   const handleLogout = () => {
-    removeCookie("access_token");
+    document.cookie =
+      "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     localStorage.clear();
     navigate("/login");
   };
@@ -33,23 +35,45 @@ function Profile() {
 
   return (
     <>
-      <div>
-        <h1>Profile Page</h1>
-        <h2>Name: {name}</h2>
-        <h2>College: {college}</h2>
-        <button onClick={handleLogout}>Logout</button>
+      <Layout>
+        <div className="pt-20 pb-32">
+          <h1 className="text-center font-medium text-lg">Profile Page</h1>
+          <h2 className="text-center text-gray-600 text-sm">Name: {name}</h2>
+          <h2 className="text-center text-gray-600 text-sm">
+            College: {college}
+          </h2>
 
-        <br />
-        <br />
-        <h1>Subscriptions</h1>
-        <hr />
-        {subscriptions.map((subscription) => (
-          <div key={subscription._id}>
-            <h3>{subscription.shop.name}</h3>
-            <p>{subscription.shop.description}</p>
+          <div className="mt-8">
+            <h1 className="text-lg font-medium">Subscriptions</h1>
+            <hr className="mt-2 border-gray-300" />
+
+            <div className="grid gap-4 mt-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+              {subscriptions.map((subscription) => (
+                <div
+                  key={subscription._id}
+                  className="bg-white rounded-lg shadow-md p-4"
+                >
+                  <h3 className="text-lg font-medium">
+                    {subscription.shop.name}
+                  </h3>
+                  <p className="mt-2 text-gray-600">
+                    {subscription.shop.description}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+
+          <div className="mt-8 text-center">
+            <button
+              className="bg-cyan-500 hover:bg-cyan-600 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </Layout>
     </>
   );
 }
